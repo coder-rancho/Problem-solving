b7e25e22eccaeb1d5394da0b200858111ea82cdc
coder-rancho <73986272+coder-rancho@users.noreply.github.com>
2022-06-27 15:45:39 +0530

    Time: 131 ms (10.41%), Space: 42 MB (72.20%) - LeetHub

--- 50-powx-n/50-powx-n.js
+++ 50-powx-n/50-powx-n.js
@@ -10,36 +10,4 @@ var myPow = function(x, n) {
         : n == -1 ? 1/x
         : n % 2 == 0 ? (myPow(x, n/2))**2
         : x * myPow(x, n-1);
-    
-    if (n == 0) return 1
-    
-    const product = pow(x, Math.abs(n))
-    
-    if (n < 0) return 1/product
-    
-    return product
-    
 };
-
-var pow = function(x, n) {
-    var x_n = {}
-    const str_xn = x.toString + "_" + n.toString
-    
-    if (n == 1) return x;
-    
-    if ( x_n[str_xn] ) return x_n[str_xn]
-    
-    let n_2 = Math.floor(n/2);
-    let result;
-    
-    if (n % 2 == 0) {
-        result = pow(x, n_2) * pow(x, n_2)
-        x_n[str_xn] = result
-    }
-    else {
-        result = pow(x, n_2) * pow(x, n_2 + 1)
-        x_n[str_xn] = result
-        
-    }
-    return result
-}
