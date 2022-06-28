0c4d2c808aa356f0f6294f389b5617344eb12ed2
coder-rancho <73986272+coder-rancho@users.noreply.github.com>
2022-06-28 14:15:29 +0530

    Time: 98 ms (34.03%), Space: 43.8 MB (19.01%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -9,17 +9,13 @@ var uniquePaths = function(m, n) {
 }
 
 var _uniquePaths = function(m, n, paths) {
-    console.log(m, n)
     const str_mn = m.toString() + "_" + n.toString();
     
     if (m < 1 || n < 1) return 0;
     if (m == 1 && n == 1) return 1;
-    
-    // console.log(paths)
-    
+        
     if ( paths[str_mn] == undefined ) paths[str_mn] = _uniquePaths(m, n-1, paths) + _uniquePaths(m-1, n, paths)
     
-    console.log(paths.str_mn)
     return paths[str_mn]
     
 }
