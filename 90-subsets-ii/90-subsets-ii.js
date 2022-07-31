94b6782560e3678d79404f3c4cc119cd63e8b38d
coder-rancho <73986272+coder-rancho@users.noreply.github.com>
2022-07-31 18:53:57 +0530

    Time: 808 ms (5.02%), Space: 58.6 MB (5.02%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    
+    function subsets(nums, l, r, subset=[]) {
+        
+        if (l > r) {
+            let copy = [...subset];
+            console.log(dp);
+            let copyStr = copy.join();
+            
+            if (dp[copyStr]) return;
+            else {
+                result.push(copy);
+                dp[copyStr] = true;
+            }
+            
+            return;
+        }
+        
+        subset.push(nums[l]);
+        
+        subsets(nums, l+1, r, subset);
+        
+        subset.pop();
+        
+        subsets(nums, l+1, r, subset);
+    }
+    
+    var result = [ ]
+    var dp = {}
+    nums.sort();
+    subsets(nums, 0, nums.length - 1);
+    
+    return result;
+};
