08e9bd5747c75c074143e9736befa3429f93d91d
Naman Vyas <coder.namanvyas@gmail.com>
2022-10-27 00:40:54 +0530

    Time: 1 ms (99.16%), Space: 44.4 MB (38.48%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -22,7 +22,7 @@ class Solution {
     }
     
     int dfs(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null || !flag) return 0;
         int left = dfs(root.left);
         int right = dfs(root.right);
         
