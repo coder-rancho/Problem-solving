ab5b72b2b9142890baa57e1632446416de422525
Naman Vyas <coder.namanvyas@gmail.com>
2022-11-01 20:37:05 +0530

    Time: 2 ms (96.02%), Space: 53.5 MB (76.30%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,48 @@
+import java.io.Console;
+import java.lang.reflect.Array;
+import java.util.*;
+
+import javax.management.Query;
+
+class Solution {
+
+    ArrayList<String> res;
+    char[] chars;
+
+    public List<String> letterCasePermutation(String s) {
+        res = new ArrayList<>();
+        chars = s.toCharArray();
+        helper(0, new char[s.length()]);
+        return res;
+    }
+
+    void helper(int indx, char[] combination) {
+
+        if (indx == combination.length) {
+            String s = new String(combination);
+            res.add(s);
+            return;
+        }
+
+        char ch = chars[indx];
+        // Compulsory call
+        combination[indx] = ch;
+        helper(indx+1, combination);
+
+        // If char is alphabet
+        if ( (int)ch > 96 && (int)ch < 123) {   // small-case alphabet
+            combination[indx] = (char)(ch - 32);
+            helper(indx+1, combination);
+        }
+
+        if ( (int)ch > 64 && (int)ch < 91) { // upper-case alphabet
+            combination[indx] = (char)(ch + 32);
+            helper(indx+1, combination);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution o = new Solution();
+        System.out.println(o.letterCasePermutation("a1b2"));
+    }
+}
